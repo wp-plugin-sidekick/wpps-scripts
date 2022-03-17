e65ba098aa5007e411c4bc839e36952c8040fd1a
Ryan Kienstra <kienstraryan@gmail.com>
2022-03-17 15:13:59 -0600

    Remove check for cwdiswppslinter, as it looks like it's never called with -c

--- lint-js.sh
+++ lint-js.sh
@@ -2,7 +2,6 @@
 
 while getopts 'c:p:n:t:f:' flag; do
 	case "${flag}" in
-		c) cwdiswppslinter=${OPTARG} ;;
 		p) plugindirname=${OPTARG} ;;
 		n) namespace=${OPTARG} ;;
 		t) textdomain=${OPTARG} ;;
@@ -10,16 +9,9 @@ while getopts 'c:p:n:t:f:' flag; do
 	esac
 done
 
-# Get the absolute path to the plugin we want to check.
-if [ "$cwdiswppslinter" = "1" ]; then
-	plugindir="$(dirname $(dirname $(dirname $(dirname $(realpath $0) ) ) ) )/$plugindirname"
-	wpcontentdir="./../../../../"
-	scriptsdir="$(dirname $(realpath $0) ) )"
-else
-	plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
-	wpcontentdir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath $0)" )" )" )" )"
-	scriptsdir="$plugindir/wpps-scripts/"
-fi
+plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
+wpcontentdir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath $0)" )" )" )" )"
+scriptsdir="$plugindir/wpps-scripts/"
 
 #Go to wp-content directory.
 cd "$wpcontentdir";
