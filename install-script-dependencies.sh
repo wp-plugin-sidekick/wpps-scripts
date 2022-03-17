57275800b12d5a147c948285b0b121afd1fcd7a6
Ryan Kienstra <kienstraryan@gmail.com>
2022-03-17 15:12:18 -0600

    See if removeing cwdiswppslinter is alright

--- install-script-dependencies.sh
+++ install-script-dependencies.sh
@@ -2,21 +2,14 @@
 
 while getopts 'c:p:n:t:f:' flag; do
 	case "${flag}" in
-		c) cwdiswppslinter=${OPTARG} ;;
 		p) plugindirname=${OPTARG} ;;
 	esac
 done
 
 # Get the absolute path to the plugin we want to check.
-if [ "$cwdiswppslinter" = "1" ]; then
-	plugindir="$(dirname $(dirname $(dirname $(dirname $(realpath $0) ) ) ) )/$plugindirname"
-	wpcontentdir="./../../../../"
-	scriptsdir="$(dirname $(realpath $0) ) )"
-else
-	plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
-	wpcontentdir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath $0)" )" )" )" )"
-	scriptsdir="$plugindir/wpps-scripts/"
-fi
+plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
+wpcontentdir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath $0)" )" )" )" )"
+scriptsdir="$plugindir/wpps-scripts/"
 
 #Go to wp-content directory.
 cd "$wpcontentdir";
