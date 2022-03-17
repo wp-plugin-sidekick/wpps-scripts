59a007d369802fc73728c63aab42901b7e8391b9
Ryan Kienstra <kienstraryan@gmail.com>
2022-03-17 15:15:30 -0600

    Remove check for cwdiswppslinter in test-js.sh

--- lint-js.sh
+++ lint-js.sh
@@ -2,6 +2,7 @@
 
 while getopts 'c:p:n:t:f:' flag; do
 	case "${flag}" in
+		c) cwdiswppslinter=${OPTARG} ;;
 		p) plugindirname=${OPTARG} ;;
 		n) namespace=${OPTARG} ;;
 		t) textdomain=${OPTARG} ;;
@@ -9,9 +10,16 @@ while getopts 'c:p:n:t:f:' flag; do
 	esac
 done
 
-plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
-wpcontentdir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath $0)" )" )" )" )"
-scriptsdir="$plugindir/wpps-scripts/"
+# Get the absolute path to the plugin we want to check.
+if [ "$cwdiswppslinter" = "1" ]; then
+	plugindir="$(dirname $(dirname $(dirname $(dirname $(realpath $0) ) ) ) )/$plugindirname"
+	wpcontentdir="./../../../../"
+	scriptsdir="$(dirname $(realpath $0) ) )"
+else
+	plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
+	wpcontentdir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath $0)" )" )" )" )"
+	scriptsdir="$plugindir/wpps-scripts/"
+fi
 
 #Go to wp-content directory.
 cd "$wpcontentdir";
--- test-js.sh
+++ test-js.sh
@@ -6,16 +6,9 @@ while getopts 'c:p:n:t:f:' flag; do
 	esac
 done
 
-# Get the absolute path to the plugin we want to check.
-if [ "$cwdiswppslinter" = "1" ]; then
-	plugindir="$(dirname $(dirname $(dirname $(dirname $(realpath $0) ) ) ) )/$plugindirname"
-	wpcontentdir="./../../../../"
-	scriptsdir="$(dirname $(realpath $0) ) )"
-else
-	plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
-	wpcontentdir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath $0)" )" )" )" )"
-	scriptsdir="$plugindir/wpps-scripts/"
-fi
+plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
+wpcontentdir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath $0)" )" )" )" )"
+scriptsdir="$plugindir/wpps-scripts/"
 
 cd "$wpcontentdir"
 sh "${scriptsdir}/install-script-dependencies.sh"
