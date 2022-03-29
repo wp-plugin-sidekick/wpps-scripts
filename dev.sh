1b317a1053a38baf2845a2658369521dab60febc
Phil Johnston <phil.johnston@wpengine.com>
2022-03-29 12:40:27 -0400

    Run npm install automatically before npm run dev

--- dev.sh
+++ dev.sh
@@ -1,19 +1,37 @@
+#!/bin/bash
+
+while getopts 'c:p:' flag; do
+	case "${flag}" in
+		c) cwdiswppslinter=${OPTARG} ;;
+		p) plugindirname=${OPTARG} ;;
+	esac
+done
+
+# Get the absolute path to the plugin we want to check.
+if [ "$cwdiswppslinter" = "1" ]; then
+	plugindir="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath "$0")" )" )" )" )/$plugindirname"
+else
+	plugindir="$(dirname "$(dirname "$(realpath "$0")" )" )"
+fi
+
 # Loop through each wp-module in the plugin.
+for DIR in $plugindir/wp-modules/*; do
+	find=$plugindir
+	replace=""
+	moduledir="${DIR/${plugindir}/${replace}}"
 
-for DIR in wp-modules/*/; do
-	# Go to the directory of this wp-module.
-	cd "$DIR";
-	echo "Module: $DIR";
-
-	# Run the "npm run dev" command in its package.json file.
-	if [[ -f "package.json" ]]
+	# If this module has a package.json file.
+	if [[ -f "$DIR/package.json" ]]
 	then
-		npm run dev &
+		# Go to the directory of this wp-module.
+		cd "$DIR";
+		# Run npm install for this module.
+		npm install;
+		
+		# Run the build script for this module.
+		npm run dev;
 	fi
 
-	# Go back to main directory, which includes the plugin modules.   
-	cd -;
-	
 done
 
 # Finish with a wait command, which lets a kill (cmd+c) kill all of the process created in this loop.
