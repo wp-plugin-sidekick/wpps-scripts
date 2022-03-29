3ca19d34e7f778ace71b07e0ac386518bb6ae165
Ryan Kienstra <kienstraryan@gmail.com>
2022-03-29 13:03:17 -0600

    Add a slash after the plugin directory

--- zip.sh
+++ zip.sh
@@ -8,10 +8,10 @@ zip_file_name="$plugin_slug.$build_version.zip"
 cd "$(dirname "$plugin_dir")"
 
 ls -alrth "$plugin_dir.zipignore"
-if [ ! -f "$plugin_dir.zipignore" ]; then
+if [ ! -f "$plugin_dir/.zipignore" ]; then
 	echo "Error: please add a .zipignore to the root of the plugin"
 	exit 1
 fi
 
-zip -r "$zip_file_name" "$plugin_slug" -x@"$plugin_dir.zipignore"
+zip -r "$zip_file_name" "$plugin_slug" -x@"$plugin_dir/.zipignore"
 mv "$zip_file_name" "$plugin_dir"
