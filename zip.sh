8dfb917b5ace629fdf99e2a0afbad474921f3207
Phil Johnston <phil.johnston@wpengine.com>
2022-03-31 14:27:32 -0400

    Use name of plugin dir as defined in step.sh

--- zip.sh
+++ zip.sh
@@ -3,16 +3,16 @@
 # Run setup.
 . ./setup.sh
 
-plugin_slug="$(basename "$plugin_dir")"
+plugin_slug="$(basename "$plugindir")"
 
-build_version=`grep 'Version:' $plugin_dir/$plugin_slug.php | cut -f4 -d' '`
+build_version=`grep 'Version:' $plugindir/$plugin_slug.php | cut -f4 -d' '`
 zip_file_name="$plugin_slug.$build_version.zip"
-cd "$(dirname "$plugin_dir")"
+cd "$(dirname "$plugindir")"
 
-if [ ! -f "$plugin_dir/.zipignore" ]; then
+if [ ! -f "$plugindir/.zipignore" ]; then
 	echo "Error: please add a .zipignore to the root of the plugin"
 	exit 1
 fi
 
-zip -r "$zip_file_name" "$plugin_slug" -x@"$plugin_dir/.zipignore"
-mv "$zip_file_name" "$plugin_dir"
+zip -r "$zip_file_name" "$plugin_slug" -x@"$plugindir/.zipignore"
+mv "$zip_file_name" "$plugindir"
