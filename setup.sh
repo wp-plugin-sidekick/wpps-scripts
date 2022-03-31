75f46f174e1e7146d54f45a2098cffa472a5970f
Phil Johnston <phil.johnston@wpengine.com>
2022-03-30 22:12:12 -0400

    Handle installing module dependencies in setup.sh

--- setup.sh
+++ setup.sh
@@ -22,3 +22,19 @@ if [ ! -d node_modules ] || [ ! -d vendor ]; then
 	npm install
 	composer install
 fi
+
+# Loop through each wp-module in the plugin, and install their dependencies.
+for DIR in $plugindir/wp-modules/*; do
+	# If this module has a package.json file.
+	if [ -f "$DIR/package.json" ]; then
+		# Go to the directory of this wp-module.
+		cd "$DIR";
+
+		# Run npm install for this module.
+		if [ ! -d node_modules ]; then
+			npm install;
+		fi
+		
+		cd -;
+	fi
+done
