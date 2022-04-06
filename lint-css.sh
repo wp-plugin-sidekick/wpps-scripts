f68aa4150f659d958f1e6ad32ead2e4a05fe9a00
Phil Johnston <phil.johnston@wpengine.com>
2022-04-05 21:10:50 -0400

    Ensure quotes around $plugindir

--- lint-css.sh
+++ lint-css.sh
@@ -5,7 +5,7 @@
 
 # Run the lint command from the wp-content directory.
 if [ "$fix" = "1" ]; then
-	npm run lint:css "$plugindir/**/css/src/*.*css"  -- --fix;
+	npm run lint:css "$plugindir"/**/css/src/*.*css  -- --fix;
 else
-	npm run lint:css "$plugindir/**/css/src/*.*css";
+	npm run lint:css "$plugindir"/**/css/src/*.*css;
 fi
