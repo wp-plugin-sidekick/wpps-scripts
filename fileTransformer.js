866dbe3725fde655251f83acde5fb235ef0f0c20
Ryan Kienstra <kienstraryan@gmail.com>
2022-06-08 18:38:17 -0500

    Require the wordpress/scripts Jest config

--- fileTransformer.js
+++ fileTransformer.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  process(sourceText, sourcePath, options) {
+  process(sourceText, sourcePath) {
     return {
       code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
     };
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 // Copied from https://jestjs.io/docs/code-transformation#examples
 module.exports = {
+  ...require('@wordpress/scripts/config/jest-unit.config'),
   transform: {
     '\\.(jpg|jpeg|png|svg)$':
       '<rootDir>/fileTransformer.js',
