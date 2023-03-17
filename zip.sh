efa76e932fdcc979aa7b22b83bc6093c840eab8c
Ryan Kienstra <kienstraryan@gmail.com>
2023-03-16 21:52:37 -0600

    Ensure .distignore begins and ends with *
    A .zipignore has to be in that format,
    though a file used as an .svnignore won't have that format.

--- zip.sh
+++ zip.sh
@@ -9,10 +9,12 @@ build_version=`grep 'Version:' "$plugindir"/$plugin_slug.php | cut -f4 -d' '`
 zip_file_name="$plugin_slug.$build_version.zip"
 cd "$(dirname "$plugindir")"
 
-if [ -f "$plugindir/.zipignore" ]; then
+ignore_file="$plugindir/.zipignore"
+if [ -f "$ignore_file" ]; then
 	ignore_file="$plugindir/.zipignore"
 elif [ -f "$plugindir/.distignore" ]; then
-	ignore_file="$plugindir/.distignore"
+	# Ensure ignore file begins and ends with *
+	sed "s/^[^*]/*&/g" "$plugindir/.distignore" | sed "s/[^*]$/&*/g" > "$ignore_file"
 else
 	echo "Error: please add a .zipignore to the root of the plugin"
 	exit 1
