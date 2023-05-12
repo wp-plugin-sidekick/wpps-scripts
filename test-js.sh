edea4d30601cab394f295ea0464f922739d49d14
Phil Johnston <phil.johnston@wpengine.com>
2023-05-12 15:55:11 -0400

    Make sure parent directories exist prior

--- test-js.sh
+++ test-js.sh
@@ -15,9 +15,14 @@ for DIR in "$plugindir"/wp-modules/*; do
 			# Move copies of node_modules required to run tests with react-test-renderer to each wp-modules node_modules directory.
 			# This allows wp-modules to keep testing dependencies out of their package.json files, and instead rely on wpps-scripts to handle that.
 			cp -r "$wpps_scripts_dir"/node_modules/react-test-renderer "$DIR"/node_modules/react-test-renderer
+			
+			mkdir -p "$DIR"/node_modules/@types
 			cp -r "$wpps_scripts_dir"/node_modules/@types/jest "$DIR"/node_modules/@types/jest
 			cp -r "$wpps_scripts_dir"/node_modules/@types/react-test-renderer "$DIR"/node_modules/@types/react-test-renderer
+			
+			mkdir -p "$DIR"/node_modules/@testing-library
 			cp -r "$wpps_scripts_dir"/node_modules/@testing-library/react "$DIR"/node_modules/@testing-library/react
+			cp -r "$wpps_scripts_dir"/node_modules/@testing-library/dom "$DIR"/node_modules/@testing-library/dom
 		fi
 		
 		cd - > /dev/null
