c17b3a762b2b9004d0e47dea98fa2af3091e0344
Ryan Kienstra <kienstraryan@gmail.com>
2023-05-19 10:56:12 -0600

    Update phpunit setup, so we can run it locally

--- phpunit.sh
+++ phpunit.sh
@@ -15,7 +15,7 @@ npx -p @wordpress/env wp-env start
 
 # Run PHPunit inside wp-env, targeting the plugin in question.
 if [ "$multisite" = "1" ]; then
-	npx -p @wordpress/env wp-env run phpunit "WP_MULTISITE=1 phpunit -c /var/www/html/wp-content/wpps-scripts/phpunit.xml.dist /var/www/html/wp-content/plugins/$plugindirname"
+	npx -p @wordpress/env wp-env run tests-cli "WP_MULTISITE=1 phpunit -c /var/www/html/wp-content/wpps-scripts/phpunit.xml.dist /var/www/html/wp-content/plugins/$plugindirname"
 else
-	npx -p @wordpress/env wp-env run phpunit "phpunit -c /var/www/html/wp-content/wpps-scripts/phpunit.xml.dist /var/www/html/wp-content/plugins/$plugindirname"
+	npx -p @wordpress/env wp-env run tests-cli "phpunit -c /var/www/html/wp-content/wpps-scripts/phpunit.xml.dist /var/www/html/wp-content/plugins/$plugindirname"
 fi
